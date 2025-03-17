997dc58f236d6af42d3100eb2dd30d16384f1af7
Ian Wafula <neocorpse@proton.me>
2025-03-17 12:35:16 +0300

    Create fetch request to complete lab

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 // Add your code here
-const formData = {
-	dogName: 'Byron',
-	dogBreed: 'Poodle',
-};
+// const formData = {
+// 	dogName: 'Byron',
+// 	dogBreed: 'Poodle',
+// };
 
-const configurationObject = {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json',
-		Accept: 'application/json',
-	},
-	body: JSON.stringify(formData),
-};
+// const configurationObject = {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		Accept: 'application/json',
+// 	},
+// 	body: JSON.stringify(formData),
+// };
 
-fetch('http://localhost:3000/dogs', configurationObject)
-	.then(function (response) {
-		return response.json();
+// fetch('http://localhost:3000/dogs', configurationObject)
+// 	.then(function (response) {
+// 		return response.json();
+// 	})
+// 	.then(function (object) {
+// 		console.log(object);
+// 	});
+
+function submitData(name, email) {
+	return fetch('http://localhost:3000/users', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ name: name, email: email }),
 	})
-	.then(function (object) {
-		console.log(object);
-	});
+		.then((res) => res.json())
+		.then((obj) => {
+			let id = obj.id;
+			document.body.textContent = id;
+			console.log(id);
+		})
+		.catch((error) => {
+			document.body.textContent = error.message;
+		});
+}
